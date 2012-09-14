1c2970381954c89a13c61d00855f0162c8381f34
Evan Coury <me@evancoury.com>
2012-09-13 18:10:11 -0700

    Initial commit

---
+++ Module.php
@@ -0,0 +1,18 @@
+<?php
+namespace EdpModuleLayouts;
+
+class Module
+{
+    public function onBootstrap($e)
+    {
+        $e->getApplication()->getEventManager()->getSharedManager()->attach('Zend\Mvc\Controller\AbstractActionController', 'dispatch', function($e) {
+            $controller      = $e->getTarget();
+            $controllerClass = get_class($controller);
+            $moduleNamespace = substr($controllerClass, 0, strpos($controllerClass, '\\'));
+            $config          = $e->getApplication()->getServiceManager()->get('config');
+            if (isset($config['module_layouts'][$moduleNamespace])) {
+                $controller->layout($config['module_layouts'][$moduleNamespace]);
+            }
+        }, 100);
+    }
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+EdpModuleLayout
+================
+Version 0.0.1 Created by Evan Coury
+
+Introduction
+------------
+
+EdpModuleLayouts is a very simple ZF2 module (less than 20 lines) that simply
+allows you to specify alternative layouts to use for each module.
+
+Usage
+-----
+
+Using EdpModuleLayouts is very, very simple. In any module config or autoloaded
+config file simply specify the following:
+
+```php
+array(
+    'module_layouts' => array(
+        'ModuleName' => 'layout/some-layout',
+    ),
+);
+```
+
+That's it!
